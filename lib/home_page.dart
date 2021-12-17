237e4da2588d1f8c85f451790edfc735f320a3af
Lukas Zoubek <zoublu@email.cz>
2021-12-17 19:04:43 +0100

    import intl pluginu

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:svoz_odpadu/constants/constants.dart';
 import 'package:svoz_odpadu/components/my_appbar.dart';
+
+import 'package:intl/date_symbol_data_local.dart';
 
 class HomePage extends StatefulWidget {
   static const id = '/homePage';
@@ -12,21 +15,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+    initializeDateFormatting(); //very important
+  }
+
   @override
   Widget build(BuildContext context) {
+    initializeDateFormatting('cs');
     return Scaffold(
       appBar: const PreferredSize(
           preferredSize: Size.fromHeight(kDMyAppBarHeight), child: MyAppBar()),
       body: Container(
-        margin: EdgeInsets.only(left: kDMargin, right: kDMargin, top: kDMargin),
+        margin: const EdgeInsets.only(
+            left: kDMargin, right: kDMargin, top: kDMargin),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             TableCalendar(
-                focusedDay: dateTimeNow,
-                firstDay: dateTimeFirstDay,
-                lastDay: dateTimeLastDay),
+              calendarFormat: CalendarFormat.month,
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              focusedDay: dateTimeNow,
+              firstDay: dateTimeFirstDay,
+              lastDay: dateTimeLastDay,
+              locale: 'cs_CZ',
+              calendarStyle: const CalendarStyle(
+                  defaultTextStyle: TextStyle(
+                      fontSize: kDFontSizeText,
+                      fontFamily: kDFontFamilyParagraph,
+                      color: Colors.black),
+                  todayDecoration: BoxDecoration(
+                    color: kDBackgroundColor,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15.0),
+                    ),
+                  ),
+                  weekendTextStyle: TextStyle(
+                      color: Colors.black,
+                      fontSize: kDFontSizeText,
+                      fontFamily: kDFontFamilyParagraph)),
+            ),
           ],
         ),
       ),
