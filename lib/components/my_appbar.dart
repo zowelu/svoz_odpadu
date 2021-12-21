3b51e80e3ccd963b1591e0556e43334e17b2fb47
Lukas Zoubek <zoublu@email.cz>
2021-12-21 12:54:22 +0100

    přidána ikona settings do my_appbar.dart

--- lib/components/my_appbar.dart
+++ lib/components/my_appbar.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:svoz_odpadu/constants/constants.dart';
 
@@ -9,10 +10,18 @@ class MyAppBar extends StatelessWidget {
     return AppBar(
       title: const Text(
         'Svoz odpadu v Dolních Kounicích',
-        style: TextStyle(fontFamily: kDFontFamilyHeader, fontSize: kDFontSizeHeader, color: kDColorTextColorBackground),
+        style: TextStyle(
+            fontFamily: kDFontFamilyHeader,
+            fontSize: kDFontSizeHeader,
+            color: kDColorTextColorBackground),
       ),
       centerTitle: true,
       backgroundColor: kDBackgroundColor,
+      actions: [
+        Padding(
+          padding: EdgeInsets.only(right: 10.0),child: Container( width: 50,child: InkWell(onTap: (){}, child: Icon(Icons.settings, ),)),
+        ),
+      ],
     );
   }
 }
