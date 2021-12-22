cb324bf3d642ea582248abcfd2b9b9e6c60407b8
Lukas Zoubek <zoublu@email.cz>
2021-12-22 16:15:50 +0100

    Vytvoření souboru notifications.dart

---
+++ lib/components/notifications.dart
@@ -0,0 +1,15 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:svoz_odpadu/utilities.dart';
+
+Future<void> createBasicNotification() async {
+  await AwesomeNotifications().createNotification(
+    content: NotificationContent(
+      id: createUniqueId(),
+      channelKey: 'basic_channel',
+      title: '${Emojis.symbols_red_exclamation_mark + Emojis.symbols_red_exclamation_mark + Emojis.symbols_red_exclamation_mark} '  ,
+      body: 'Nezapomeňte vyvést popelnice ${Emojis.symbols_red_exclamation_mark + Emojis.symbols_red_exclamation_mark + Emojis.symbols_red_exclamation_mark}',
+      bigPicture: 'assets://assets/images/notification_map.png',
+      notificationLayout: NotificationLayout.BigPicture,
+    ),
+  );
+}
