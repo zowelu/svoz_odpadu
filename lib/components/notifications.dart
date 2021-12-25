6e62554835d32fcd586c7eae1319878e6cf8adf1
Lukáš Zoubek <zoublu@email.cz>
2021-12-25 22:12:49 +0100

    Implementace createSheduledReminderNotification v notifications.dart

--- lib/components/notifications.dart
+++ lib/components/notifications.dart
@@ -6,10 +6,35 @@ Future<void> createBasicNotification() async {
     content: NotificationContent(
       id: createUniqueId(),
       channelKey: 'basic_channel',
-      title: '${Emojis.symbols_red_exclamation_mark + Emojis.symbols_red_exclamation_mark + Emojis.symbols_red_exclamation_mark} '  ,
-      body: 'Nezapomeňte vyvést popelnice ${Emojis.symbols_red_exclamation_mark + Emojis.symbols_red_exclamation_mark + Emojis.symbols_red_exclamation_mark}',
+      title:
+          '${Emojis.symbols_red_exclamation_mark + Emojis.symbols_red_exclamation_mark + Emojis.symbols_red_exclamation_mark} ',
+      body:
+          'Nezapomeňte vyvést popelnice ${Emojis.symbols_red_exclamation_mark + Emojis.symbols_red_exclamation_mark + Emojis.symbols_red_exclamation_mark}',
       bigPicture: 'asset://assets/images/notification_map.png',
       notificationLayout: NotificationLayout.BigPicture,
+    ),
+  );
+}
+
+Future<void> createScheduledReminderNotification(
+    NotificationWeekAndTime notificationScheduled) async {
+  await AwesomeNotifications().createNotification(
+    content: NotificationContent(
+        id: createUniqueId(),
+        channelKey: 'scheduled_channel',
+        title: '${Emojis.symbols_warning} Popenice!!!',
+        body: 'Dnes se vyváží popelnice. Nezapomeňte!!!',
+        notificationLayout: NotificationLayout.Default),
+    actionButtons: [
+      NotificationActionButton(key: 'MARK_DONE', label: 'Hotovo')
+    ],
+    schedule: NotificationCalendar(
+      repeats: true,
+      weekday: notificationScheduled.dayOfTheWeek,
+      hour: notificationScheduled.timeOfDay.hour,
+      minute: notificationScheduled.timeOfDay.minute,
+      second: 0,
+      millisecond: 0,
     ),
   );
 }
