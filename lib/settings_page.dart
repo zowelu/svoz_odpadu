39e0ce3d816270a5ed85bcdbd23103ec01cd5d25
Lukas Zoubek <zoublu@email.cz>
2021-12-27 19:02:46 +0100

    úpravga vzhledu settings_page.dart

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:svoz_odpadu/components/notifications.dart';
 import 'package:svoz_odpadu/components/text_header.dart';
+import 'package:svoz_odpadu/components/text_normal.dart';
 import 'package:svoz_odpadu/constants/constants.dart';
 import 'package:svoz_odpadu/components/my_appbar.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:svoz_odpadu/utilities.dart';
 import 'package:svoz_odpadu/components/button_settings.dart';
+import 'package:svoz_odpadu/components/global_var.dart';
 
 class SettingsPage extends StatefulWidget {
   static const id = '/homePage';
@@ -25,44 +27,77 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     initializeDateFormatting('cs');
     return Scaffold(
-      appBar: const PreferredSize(
-          preferredSize: Size.fromHeight(kDMyAppBarHeight), child: MyAppBar()),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            color: kDBackgroundColorCalendar,
-            width: double.infinity,
-            child: const Center(
-              child: TextHeader(text: 'Nastavení'),
-            ),
-          ),
-          Column(
-            children: [
-              ButtonSettings(
-                onTap: () async {
-                  NotificationWeekAndTime? pickedShedule =
-                      await pickSchedule(context);
+        appBar: const PreferredSize(
+            preferredSize: Size.fromHeight(kDMyAppBarHeight),
+            child: MyAppBar()),
+        body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                color: kDBackgroundColorCalendar,
+                width: double.infinity,
+                height: kDMyAppBarHeight,
+                child: const Center(
+                  child: TextHeader(text: 'Nastavení'),
+                ),
+              ),
+              Flexible(
+                flex: 2,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    TextNormal(
+                        text: activeSheduledReminder
+                            ? 'Máte zapnuté upozorňování'
+                            : 'Nemáte žádná zapnutá upozornění'),
+                    ButtonSettings(
+                      onTap: () async {
+                        NotificationWeekAndTime? pickedShedule =
+                            await pickSchedule(context);
 
-                  if (pickedShedule != null) {
-                    createScheduledReminderNotification(pickedShedule);
-                  }
-                },
-                title: 'Nastavit upozornění',
-                subtitle:
-                    'Nastavíte upozornění na zvolený den a čas každý týden',
-                icon: Icons.notifications,
+                        if (pickedShedule != null) {
+                          createScheduledReminderNotification(pickedShedule);
+                        }
+                        setState(() {
+                          activeSheduledReminder = true;
+                        });
+                      },
+                      title: 'Nastavit upozornění',
+                      subtitle:
+                          'Nastavíte upozornění na zvolený den a čas každý týden',
+                      icon: Icons.notifications,
+                    ),
+                    ButtonSettings(
+                      onTap: () {
+                        cancelScheduledNotifications;
+                        setState(() {
+                          activeSheduledReminder = false;
+                        });
+                      },
+                      title: 'Zrušit upozornění',
+                      subtitle: 'Zrušíte všechna nastavená upozornění',
+                      icon: Icons.notifications_off,
+                    ),
+                  ],
+                ),
               ),
-              ButtonSettings(
-                onTap: cancelScheduledNotifications,
-                title: 'Zrušit upozornění',
-                subtitle: 'Zrušíte všechna nastavená upozornění',
-                icon: Icons.notifications_off,
-              ),
-            ],
-          )
-        ],
-      ),
-    );
+              Flexible(
+                flex: 1,
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(color: kDBackgroundColor),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text('ahoj'),
+                    ],
+                  ),
+                ),
+              )
+            ]));
   }
 }
