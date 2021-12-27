c70f8765aa60798cca16dc8dbc97553ad2dc589e
Lukas Zoubek <zoublu@email.cz>
2021-12-27 20:19:09 +0100

    změna vzhledu settings_page.dart

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -27,77 +27,118 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     initializeDateFormatting('cs');
     return Scaffold(
-        appBar: const PreferredSize(
-            preferredSize: Size.fromHeight(kDMyAppBarHeight),
-            child: MyAppBar()),
-        body: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                color: kDBackgroundColorCalendar,
-                width: double.infinity,
-                height: kDMyAppBarHeight,
-                child: const Center(
-                  child: TextHeader(text: 'Nastavení'),
-                ),
-              ),
-              Flexible(
-                flex: 2,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    TextNormal(
+      appBar: const PreferredSize(
+          preferredSize: Size.fromHeight(kDMyAppBarHeight), child: MyAppBar()),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            color: kDBackgroundColorCalendar,
+            width: double.infinity,
+            height: kDMyAppBarHeight,
+            child: const Center(
+              child: TextHeader(text: 'Nastavení'),
+            ),
+          ),
+          Flexible(
+            flex: 8,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                /*TextNormal(
                         text: activeSheduledReminder
-                            ? 'Máte zapnuté upozorňování'
-                            : 'Nemáte žádná zapnutá upozornění'),
-                    ButtonSettings(
-                      onTap: () async {
-                        NotificationWeekAndTime? pickedShedule =
-                            await pickSchedule(context);
+                            ? 'Máte zapnuté upozorňování.\n$selectedDayGlobal v ${selectedTimeOfDayGlobal.toString().replaceAll('TimeOfDay', '').replaceAll('(', '').replaceAll(')', '')}'
+                            : 'Nemáte žádná zapnutá upozornění'),*/
+                ButtonSettings(
+                  onTap: () async {
+                    NotificationWeekAndTime? pickedShedule =
+                        await pickSchedule(context);
 
-                        if (pickedShedule != null) {
-                          createScheduledReminderNotification(pickedShedule);
-                        }
-                        setState(() {
-                          activeSheduledReminder = true;
-                        });
-                      },
-                      title: 'Nastavit upozornění',
-                      subtitle:
-                          'Nastavíte upozornění na zvolený den a čas každý týden',
-                      icon: Icons.notifications,
-                    ),
-                    ButtonSettings(
-                      onTap: () {
-                        cancelScheduledNotifications;
-                        setState(() {
-                          activeSheduledReminder = false;
-                        });
-                      },
-                      title: 'Zrušit upozornění',
-                      subtitle: 'Zrušíte všechna nastavená upozornění',
-                      icon: Icons.notifications_off,
-                    ),
-                  ],
+                    if (pickedShedule != null) {
+                      createScheduledReminderNotification(pickedShedule);
+                    }
+                    setState(() {
+                      activeSheduledReminder = true;
+                    });
+                  },
+                  title: 'Nastavit upozornění',
+                  subtitle:
+                      'Nastavíte upozornění na zvolený den a čas každý týden',
+                  icon: Icons.notifications,
                 ),
-              ),
-              Flexible(
-                flex: 1,
-                child: Container(
-                  width: double.infinity,
-                  decoration: BoxDecoration(color: kDBackgroundColor),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Text('ahoj'),
-                    ],
-                  ),
+                SizedBox(
+                  height: 5,
                 ),
-              )
-            ]));
+                SizedBox(
+                  height: 10,
+                ),
+                ButtonSettings(
+                  onTap: () {
+                    cancelScheduledNotifications;
+                    setState(() {
+                      activeSheduledReminder = false;
+                      selectedDayGlobal = '';
+                      selectedTimeOfDayGlobal = TimeOfDay(hour: 0, minute: 0);
+                    });
+                  },
+                  title: 'Zrušit upozornění',
+                  subtitle: 'Zrušíte všechna nastavená upozornění',
+                  icon: Icons.notifications_off,
+                ),
+              ],
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Column(mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+            activeSheduledReminder
+                ? Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.only(topLeft: Radius.circular(10.0),topRight: Radius.circular(10.0),),
+                        color: Colors.redAccent,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            spreadRadius: 1,
+                            blurRadius: 2,
+                            offset:
+                                Offset(0, 1), // changes position of shadow
+                          ),
+                        ]),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Máte zapnuté upozorňování každý týden v:',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: kDFontFamilyParagraph,
+                            fontSize: 14,
+                          ),
+                        ),
+                        Text(
+                          '$selectedDayGlobal v ${selectedTimeOfDayGlobal.toString().replaceAll('TimeOfDay', '').replaceAll('(', '').replaceAll(')', '')}',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: kDFontFamilyParagraph,
+                            fontSize: kDFontSizeText,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                : Text(''),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
