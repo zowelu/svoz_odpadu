4ce1ea535d58cdff1acdefc0e61266cbe2f53398
Lukáš Zoubek <zoublu@email.cz>
2021-12-27 17:41:37 +0100

    Změna rozestavení v settings_page.dart

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:svoz_odpadu/components/notifications.dart';
+import 'package:svoz_odpadu/components/text_header.dart';
 import 'package:svoz_odpadu/constants/constants.dart';
 import 'package:svoz_odpadu/components/my_appbar.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:svoz_odpadu/utilities.dart';
+import 'package:svoz_odpadu/components/button_settings.dart';
 
 class SettingsPage extends StatefulWidget {
   static const id = '/homePage';
@@ -19,37 +21,47 @@ class _SettingsPageState extends State<SettingsPage> {
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
     initializeDateFormatting('cs');
     return Scaffold(
       appBar: const PreferredSize(
           preferredSize: Size.fromHeight(kDMyAppBarHeight), child: MyAppBar()),
-      body: Container(
-        padding: const EdgeInsets.only(top: kDMarginLarger),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            IconButton(
-              onPressed: createBasicNotification,
-              icon: const Icon(Icons.notifications),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            color: kDBackgroundColorCalendar,
+            width: double.infinity,
+            child: const Center(
+              child: TextHeader(text: 'Nastavení'),
             ),
-            IconButton(
-              onPressed: () async {NotificationWeekAndTime? pickedShedule = await pickSchedule(context);
+          ),
+          Column(
+            children: [
+              ButtonSettings(
+                onTap: () async {
+                  NotificationWeekAndTime? pickedShedule =
+                      await pickSchedule(context);
 
-              if(pickedShedule != null){
-                createScheduledReminderNotification(pickedShedule);
-              }},
-              icon: const Icon(Icons.notifications_active),
-            ),
-            IconButton(
-              onPressed: cancelScheduledNotifications,
-              icon: const Icon(Icons.notifications_off),
-            ),
-          ],
-        ),
+                  if (pickedShedule != null) {
+                    createScheduledReminderNotification(pickedShedule);
+                  }
+                },
+                title: 'Nastavit upozornění',
+                subtitle:
+                    'Nastavíte upozornění na zvolený den a čas každý týden',
+                icon: Icons.notifications,
+              ),
+              ButtonSettings(
+                onTap: cancelScheduledNotifications,
+                title: 'Zrušit upozornění',
+                subtitle: 'Zrušíte všechna nastavená upozornění',
+                icon: Icons.notifications_off,
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
