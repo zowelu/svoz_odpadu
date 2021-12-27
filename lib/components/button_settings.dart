d72537dc7f0148f6dfc3c21801987e6e996f228f
Lukáš Zoubek <zoublu@email.cz>
2021-12-27 17:40:59 +0100

    vytvoření buttonSettngs v button_settings.dartpřidání icon do konstruktora v button_settings.dart

--- lib/components/button_settings.dart
+++ lib/components/button_settings.dart
@@ -2,56 +2,63 @@ import 'package:flutter/material.dart';
 import 'package:svoz_odpadu/constants/constants.dart';
 
 class ButtonSettings extends StatelessWidget {
-   const ButtonSettings({Key? key,
-    required this.onTap, required this.title, required this.subtitle
-  }) : super(key: key);
+  const ButtonSettings(
+      {Key? key,
+      required this.onTap,
+      required this.title,
+      required this.subtitle, required this.icon})
+      : super(key: key);
 
   final onTap;
   final String title;
   final String subtitle;
+  final IconData icon;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Container(
-          decoration: const BoxDecoration(
-            shape: BoxShape.rectangle,
-            borderRadius: BorderRadius.all(Radius.circular(20)),
-            color: kDBackgroundColor,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black12,
-                spreadRadius: 1,
-                blurRadius: 2,
-                offset: Offset(0, 1), // changes position of shadow
-              ),
-            ],
-          ),
-          width: MediaQuery.of(context).size.width / 100 * 80,
-          child: ListTile(
-            subtitle: Text(
-              subtitle,
-              style: const TextStyle(
-                color: Colors.white,
-                fontFamily: kDFontFamilyParagraph,
-                fontSize: 14,
-              ),
+    return Container(
+      margin: EdgeInsets.all(kDMargin),
+      child: InkWell(
+        onTap: onTap,
+        child: Container(
+            decoration: const BoxDecoration(
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              color: kDBackgroundColor,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  spreadRadius: 1,
+                  blurRadius: 2,
+                  offset: Offset(0, 1), // changes position of shadow
+                ),
+              ],
             ),
-            title: Text(
-              title,
-              style: const TextStyle(
-                  fontSize: kDFontSizeHeader,
+            width: MediaQuery.of(context).size.width / 100 * 80,
+            child: ListTile(
+              subtitle: Text(
+                subtitle,
+                style: const TextStyle(
                   color: Colors.white,
                   fontFamily: kDFontFamilyParagraph,
-                  fontWeight: FontWeight.bold),
-            ),
-            leading: const Icon(
-              Icons.notifications,
-              color: Colors.white,
-              size: 35,
-            ),
-          )),
+                  fontSize: 14,
+                ),
+              ),
+              title: Text(
+                title,
+                style: const TextStyle(
+                    fontSize: kDFontSizeHeader,
+                    color: Colors.white,
+                    fontFamily: kDFontFamilyParagraph,
+                    fontWeight: FontWeight.bold),
+              ),
+              leading: Icon(
+                icon,
+                color: Colors.white,
+                size: 35,
+              ),
+            ),),
+      ),
     );
   }
 }
