61035626309a584d30b40c435f55094cee4b0c87
Lukáš Zoubek <zoublu@email.cz>
2021-12-27 17:26:34 +0100

    vytvoření buttonSettngs v button_settings.dart

---
+++ lib/components/button_settings.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:svoz_odpadu/constants/constants.dart';
+
+class ButtonSettings extends StatelessWidget {
+   const ButtonSettings({Key? key,
+    required this.onTap, required this.title, required this.subtitle
+  }) : super(key: key);
+
+  final onTap;
+  final String title;
+  final String subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+          decoration: const BoxDecoration(
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            color: kDBackgroundColor,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                spreadRadius: 1,
+                blurRadius: 2,
+                offset: Offset(0, 1), // changes position of shadow
+              ),
+            ],
+          ),
+          width: MediaQuery.of(context).size.width / 100 * 80,
+          child: ListTile(
+            subtitle: Text(
+              subtitle,
+              style: const TextStyle(
+                color: Colors.white,
+                fontFamily: kDFontFamilyParagraph,
+                fontSize: 14,
+              ),
+            ),
+            title: Text(
+              title,
+              style: const TextStyle(
+                  fontSize: kDFontSizeHeader,
+                  color: Colors.white,
+                  fontFamily: kDFontFamilyParagraph,
+                  fontWeight: FontWeight.bold),
+            ),
+            leading: const Icon(
+              Icons.notifications,
+              color: Colors.white,
+              size: 35,
+            ),
+          )),
+    );
+  }
+}
