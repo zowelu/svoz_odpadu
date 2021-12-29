4077cd4115bb57d426ee5721c2c36861eb68bf50
Lukas Zoubek <zoublu@email.cz>
2021-12-29 01:16:51 +0100

    přidání about_app_dialog.dart do aplikace

---
+++ lib/components/about_app_dialog.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:svoz_odpadu/constants/constants.dart';
+import 'package:svoz_odpadu/components/open_url_in_browser.dart';
+
+class AboutAppDialog extends StatefulWidget {
+  const AboutAppDialog({Key? key}) : super(key: key);
+
+
+
+  @override
+  _AboutAppDialogState createState() => _AboutAppDialogState();
+}
+
+class _AboutAppDialogState extends State<AboutAppDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(kDPadding),
+      ),
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      child: contentBox(context),
+    );
+  }
+
+  contentBox(context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.only(
+              left: kDPadding,
+              top: 70.0 + kDPadding,
+              right: kDPadding,
+              bottom: kDPadding),
+          margin: const EdgeInsets.only(top: kDMarginLarger),
+          decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(kDPadding),
+              boxShadow: const [
+                BoxShadow(
+                    color: Colors.black, offset: Offset(0, 10), blurRadius: 10),
+              ]),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const Text(
+                'Město památek',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+              ),
+              const Text(
+                'verze: $versionApp',
+                style: TextStyle(fontSize: 14),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: kDMarginLarger,
+              ),
+              const Text(
+                'Tato aplikace je poskytnuta zdarma Městu Dolní Kounice.',
+                style: TextStyle(fontSize: 14),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: kDMargin,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Aplikaci vytvořil',
+                    style: TextStyle(fontSize: 14),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    width: kDMargin,
+                  ),
+                  GestureDetector(
+                    onTap: () async {
+                      //po stisknutí otevře stránku v externím prohlížeči
+                      OpenUrlInBrowser().openUrl('https://webstrong.cz/');
+                    },
+                    child: Image.asset('assets/images/webstrong-logo.png',
+                        height: 25),
+                  ),
+                ],
+              ),
+              InkWell(
+                child: const Text(
+                  'www.webstrong.cz',
+                  style: TextStyle(
+                      fontSize: 14, decoration: TextDecoration.underline),
+                  textAlign: TextAlign.center,
+                ),
+                onTap: () async {
+                  //po stisknutí otevře stránku v externím prohlížeči
+                  OpenUrlInBrowser().openUrl('https://webstrong.cz/');
+                },
+              ),
+              const SizedBox(
+                height: 22,
+              ),
+              const Text(
+                'Text, fotografie jsou použity\nz dostupných pramenů\na zdrojů od TIC Dolní Kounice\na z webových stránek www.DolniKounice.cz',
+                style: TextStyle(fontSize: 14),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 22,
+              ),
+              const Text(
+                'Audiozáznam je profesionálně namluven\npanem Martinem Karlíkem,\nmoderátorem ČRo Radiožurnál.',
+                style: TextStyle(fontSize: 14),
+                textAlign: TextAlign.center,
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text(
+                      'Zavřít',
+                      style: TextStyle(fontSize: 18),
+                    )),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          left: 100,
+          right: 100,
+          child: CircleAvatar(
+            backgroundColor: Colors.transparent,
+            radius: 50.0,
+            child: ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(50.0)),
+              child: Image.asset('assets/images/dk_znak_icon.png'),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
