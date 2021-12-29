fbe4d5d8c643b157b649e952cd992e6125ef6150
Lukas Zoubek <zoublu@email.cz>
2021-12-29 01:17:01 +0100

    přidání open_url_in_browser.dart do aplikace

---
+++ lib/components/open_url_in_browser.dart
@@ -0,0 +1,21 @@
+import 'package:url_launcher/url_launcher.dart';
+
+//funkce otevírající stránku v externím prohlížeči
+class OpenUrlInBrowser {
+  //String _launchURL ='https://www.dolnikounice.cz/klaster-rosa-coeli/d-78777/p1=4774';
+
+  Future<void> _launchInBrowser(String url) async {
+    var urllaunchable =
+    await canLaunch(url); //canLaunch is from url_launcher package
+    if (urllaunchable) {
+      await launch(
+          url, forceSafariVC: false, forceWebView: false, headers: <String, String>{'header_key': 'header_value'}); //launch is from url_launcher package to launch URL
+    } else {
+      print("URL can't be launched.");
+    }
+  }
+
+  void openUrl(String launchURL) {
+    _launchInBrowser(launchURL);
+  }
+}
