65375eac9ff5fa1cbb0ec8076307603d411c76a8
Lukas Zoubek <zoublu@email.cz>
2022-01-12 19:43:12 +0100

    odstranění různé createSheduledReminderNotification v notifications.dart

--- lib/components/notifications.dart
+++ lib/components/notifications.dart
@@ -16,43 +16,27 @@ Future<void> createBasicNotification() async {
   );
 }
 
+///vytvoří notifikaci dle parametrů
 Future<void> createScheduledReminderNotification(
-    NotificationWeekAndTime notificationScheduled) async {
+    NotificationWeekAndTime notificationScheduled,
+    String channelKey,
+    String title,
+    String body,
+    String bigPicture,
+    int day,
+    int month,
+    int year) async {
   await AwesomeNotifications().createNotification(
     content: NotificationContent(
       id: createUniqueId(),
-      channelKey: 'scheduled_channel',
-      title: '${Emojis.symbols_red_exclamation_mark} Popelnice ${Emojis.symbols_red_exclamation_mark}',
-      body: 'Dnes se vyváží popelnice. Nezapomeňte${Emojis.symbols_red_exclamation_mark}',
-      bigPicture: 'asset://assets/images/popelnice.jpg',
-      notificationLayout: NotificationLayout.BigPicture,
-      wakeUpScreen: true,
-    ),
-    actionButtons: [
-      NotificationActionButton(key: 'MARK_DONE', label: 'Hotovo')
-    ],
-    schedule: NotificationCalendar(
-      repeats: true,
-      weekday: notificationScheduled.dayOfTheWeek,
-      hour: notificationScheduled.timeOfDay.hour,
-      minute: notificationScheduled.timeOfDay.minute,
-      second: 0,
-      millisecond: 0,
-      preciseAlarm: true,
-      allowWhileIdle: true,
-    ),
-  );
-}
-
-Future<void> createScheduledReminderNotificationPlastic(
-    NotificationWeekAndTime notificationScheduled,int day,int month,int year) async {
-  await AwesomeNotifications().createNotification(
-    content: NotificationContent(
-      id: createUniqueId(),
-      channelKey: 'Plast',
-      title: '${Emojis.symbols_red_exclamation_mark} Popelnice  - Plast a nápojový karton + drobné kovy${Emojis.symbols_red_exclamation_mark}',
-      body: 'Dnes se vyváží popelnice - Plast a nápojový karton + drobné kovy. Nezapomeňte${Emojis.symbols_red_exclamation_mark}',
-      bigPicture: 'asset://assets/images/popelnice.jpg',
+      //channelKey: 'scheduled_channel',
+      channelKey: channelKey,
+      //title: '${Emojis.symbols_red_exclamation_mark} Popelnice ${Emojis.symbols_red_exclamation_mark}',
+      title: title,
+      //body: 'Dnes se vyváží popelnice. Nezapomeňte${Emojis.symbols_red_exclamation_mark}',
+      body: body,
+      //bigPicture: 'asset://assets/images/popelnice.jpg',
+      bigPicture: bigPicture,
       notificationLayout: NotificationLayout.BigPicture,
       wakeUpScreen: true,
     ),
@@ -73,98 +57,6 @@ Future<void> createScheduledReminderNotificationPlastic(
     ),
   );
 }
-
-Future<void> createScheduledReminderNotificationBio(
-    NotificationWeekAndTime notificationScheduled,int day,int month,int year) async {
-  await AwesomeNotifications().createNotification(
-    content: NotificationContent(
-      id: createUniqueId(),
-      channelKey: 'Bioodpad',
-      title: '${Emojis.symbols_red_exclamation_mark} Popelnice - Bioodpad${Emojis.symbols_red_exclamation_mark}',
-      body: 'Dnes se vyváží popelnice - Bioodpad. Nezapomeňte${Emojis.symbols_red_exclamation_mark}',
-      bigPicture: 'asset://assets/images/popelnice.jpg',
-      notificationLayout: NotificationLayout.BigPicture,
-      wakeUpScreen: true,
-    ),
-    actionButtons: [
-      NotificationActionButton(key: 'MARK_DONE', label: 'Hotovo')
-    ],
-    schedule: NotificationCalendar(
-      repeats: false,
-      day: day,
-      month: month,
-      year: year,
-      weekday: notificationScheduled.dayOfTheWeek,
-      hour: notificationScheduled.timeOfDay.hour,
-      minute: notificationScheduled.timeOfDay.minute,
-      second: 0,
-      millisecond: 0,
-      preciseAlarm: true,
-      allowWhileIdle: true,
-    ),
-  );
-}
-
-Future<void> createScheduledReminderNotificationPaper(
-    NotificationWeekAndTime notificationScheduled,int day,int month,int year) async {
-  await AwesomeNotifications().createNotification(
-    content: NotificationContent(
-      id: createUniqueId(),
-      channelKey: 'Papír',
-      title: '${Emojis.symbols_red_exclamation_mark} Popelnice - Papír${Emojis.symbols_red_exclamation_mark}',
-      body: 'Dnes se vyváží popelnice - Papír. Nezapomeňte${Emojis.symbols_red_exclamation_mark}',
-      bigPicture: 'asset://assets/images/popelnice.jpg',
-      notificationLayout: NotificationLayout.BigPicture,
-      wakeUpScreen: true,
-    ),
-    actionButtons: [
-      NotificationActionButton(key: 'MARK_DONE', label: 'Hotovo')
-    ],
-    schedule: NotificationCalendar(
-      repeats: false,
-      day: day,
-      month: month,
-      year: year,
-      hour: notificationScheduled.timeOfDay.hour,
-      minute: notificationScheduled.timeOfDay.minute,
-      second: 0,
-      millisecond: 0,
-      preciseAlarm: true,
-      allowWhileIdle: true,
-    ),
-  );
-}
-
-Future<void> createScheduledReminderNotificationMixed(
-    NotificationWeekAndTime notificationScheduled, int day, int month,int year) async {
-  await AwesomeNotifications().createNotification(
-    content: NotificationContent(
-      id: createUniqueId(),
-      channelKey: 'Směsný odpad',
-      title: '${Emojis.symbols_red_exclamation_mark} Popelnice - Směsný odpad ${Emojis.symbols_red_exclamation_mark}',
-      body: 'Dnes se vyváží popelnice - Směsný odpad. Nezapomeňte${Emojis.symbols_red_exclamation_mark}',
-      bigPicture: 'asset://assets/images/popelnice.jpg',
-      notificationLayout: NotificationLayout.BigPicture,
-      wakeUpScreen: true,
-    ),
-    actionButtons: [
-      NotificationActionButton(key: 'MARK_DONE', label: 'Hotovo')
-    ],
-    schedule: NotificationCalendar(
-      repeats: false,
-      day: day,
-      month: month,
-      year: year,
-      hour: notificationScheduled.timeOfDay.hour,
-      minute: notificationScheduled.timeOfDay.minute,
-      second: 0,
-      millisecond: 0,
-      preciseAlarm: true,
-      allowWhileIdle: true,
-    ),
-  );
-}
-
 
 ///zruší všechny plánované notifikace
 Future<void> cancelScheduledNotificationsAll() async {
