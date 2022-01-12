741d2720d014d49b391bff07d6a7cd8eb14daefd
Lukas Zoubek <zoublu@email.cz>
2022-01-12 20:10:43 +0100

    vytvořeny proměnné pro uchování času aktivních notifikací v global_var.dart

--- lib/components/global_var.dart
+++ lib/components/global_var.dart
@@ -4,3 +4,12 @@ bool activeSheduledReminder = false;
 String selectedDayGlobal = '';
 TimeOfDay selectedTimeOfDayGlobal = const TimeOfDay(hour: 0, minute: 0);
 String currentPage = '';
+
+TimeOfDay? plasticReminderTime;
+int? plasticReminderday;
+TimeOfDay? bioReminderTime;
+int? bioReminderday;
+TimeOfDay? paperReminderTime;
+int? paperReminderday;
+TimeOfDay? mixedReminderTime;
+int? mixedReminderday;
