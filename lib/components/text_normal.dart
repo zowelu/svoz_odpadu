461e6075e989eaf28451ff4b43610aed03f44e20
Lukas Zoubek <zoublu@email.cz>
2022-01-13 20:32:53 +0100

    přidán fontSize do konstruktoru v text_normal.dart

--- lib/components/text_normal.dart
+++ lib/components/text_normal.dart
@@ -2,18 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:svoz_odpadu/constants/constants.dart';
 
 class TextNormal extends StatelessWidget {
-  const TextNormal({Key? key, required this.text, this.color = Colors.white, this.fontWeight = FontWeight.normal}) : super(key: key);
+  const TextNormal({Key? key, required this.text, this.color = Colors.white, this.fontWeight = FontWeight.normal, this.fontSize = kDFontSizeText}) : super(key: key);
 
   final String? text;
   final Color? color;
   final FontWeight fontWeight;
+  final double fontSize;
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text!,
       style: TextStyle(
-        fontSize: kDFontSizeText,
+        fontSize: fontSize,
         fontFamily: kDFontFamilyParagraph,
         color: color,
         fontWeight: fontWeight,
