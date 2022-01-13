39ccf0a7ecd499576e22781a9db34c1bc7fc0cdd
Lukas Zoubek <zoublu@email.cz>
2022-01-13 20:33:36 +0100

    úprava reminder_icon_on_off.dart

--- lib/components/reminder_icon_on_off.dart
+++ lib/components/reminder_icon_on_off.dart
@@ -3,17 +3,17 @@ import 'package:flutter/material.dart';
 class IconReminderOnOff extends StatelessWidget {
   const IconReminderOnOff({
     Key? key,
-    required this.isSwitchedPlastic,
+    required this.isSwitched,
   }) : super(key: key);
 
-  final bool isSwitchedPlastic;
+  final bool isSwitched;
 
   @override
   Widget build(BuildContext context) {
     return Flexible(
       flex: 1,
       fit: FlexFit.tight,
-      child: isSwitchedPlastic
+      child: isSwitched
           ? const Icon(
         Icons.notifications_active,
         color: Colors.white,
