39d2ffd37d39ecb628b98050fdae30ea4737ee36
Lukas Zoubek <zoublu@email.cz>
2022-01-16 21:30:21 +0100

    koment global_var.dart dat

--- lib/constants/global_var.dart
+++ lib/constants/global_var.dart
@@ -7,7 +7,12 @@ String selectedDayGlobal = '';
 TimeOfDay selectedTimeOfDayGlobal = const TimeOfDay(hour: 0, minute: 0);
 String currentPage = '';
 
-TimeOfDay? plasticReminderTime = TimeOfDay(hour: 00, minute: 00);
+bool isSwitchedPlastic = false;
+bool isSwitchedBio = false;
+bool isSwitchedPaper = false;
+bool isSwitchedMixed = false;
+
+/*TimeOfDay? plasticReminderTime = TimeOfDay(hour: 00, minute: 00);
 int? plasticReminderday = 0;
 String? plasticSelectedDay = 'V daný den';
 TimeOfDay? bioReminderTime = TimeOfDay(hour: 00, minute: 00);
@@ -18,4 +23,17 @@ int? paperReminderday = 0;
 String? paperSelectedDay = 'V daný den';
 TimeOfDay? mixedReminderTime = TimeOfDay(hour: 00, minute: 00);
 int? mixedReminderday = 0;
-String? mixedSelectedDay = 'V daný den';
+String? mixedSelectedDay = 'V daný den';*/
+
+TimeOfDay? plasticReminderTime;
+int? plasticReminderday;
+String? plasticSelectedDay;
+TimeOfDay? bioReminderTime;
+int? bioReminderday;
+String? bioSelectedDay;
+TimeOfDay? paperReminderTime;
+int? paperReminderday;
+String? paperSelectedDay;
+TimeOfDay? mixedReminderTime;
+int? mixedReminderday;
+String? mixedSelectedDay;
