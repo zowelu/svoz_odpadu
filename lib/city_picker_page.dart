dbb23a17310678fccca775cdbf94ba125dd0eb10
Lukas Zoubek <zoublu@email.cz>
2022-01-17 20:38:31 +0100

    vytvoření city_picker_page.dart

---
+++ lib/city_picker_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:svoz_odpadu/components/my_appbar.dart';
+import 'package:svoz_odpadu/constants/constants.dart';
+
+class CityPickerPage extends StatefulWidget {
+  const CityPickerPage({Key? key}) : super(key: key);
+  static const id = '/cityPickerPage';
+
+  @override
+  _CityPickerPageState createState() => _CityPickerPageState();
+}
+
+class _CityPickerPageState extends State<CityPickerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const PreferredSize(
+          preferredSize: Size.fromHeight(kDMarginLarger), child: MyAppBar()),
+      body: Container(
+        padding:
+            EdgeInsets.only(top: MediaQuery.of(context).size.height / 100 * 2),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(10), width: double.infinity,
+              decoration: BoxDecoration(color: kDBackgroundColor, boxShadow: [
+                BoxShadow(
+                    color: Colors.blueGrey,
+                    blurRadius: 5.0,
+                    offset: Offset(0, 0),
+                    spreadRadius: 5),
+              ]),
+              //margin: const EdgeInsets.only(top: kDMarginLarger),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width / 100 * 75,
+                    child: Container(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
