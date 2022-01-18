eef1b60596003721c275e4e5bd20a5ef5bcf9ae5
Lukas Zoubek <zoublu@email.cz>
2022-01-18 19:44:05 +0100

    import reminder icon a reminder time do list_tile_of_waste_notification.dart

--- lib/components/list_tile_of_waste_notification.dart
+++ lib/components/list_tile_of_waste_notification.dart
@@ -1,48 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:svoz_odpadu/components/reminder_icon_on_off.dart';
+import 'package:svoz_odpadu/components/reminder_time_and_date.dart';
 import 'package:svoz_odpadu/variables/constants.dart';
+import 'package:svoz_odpadu/variables/global_var.dart';
 
 class ListTileOfWasteNotification extends StatelessWidget {
   // ignore: use_key_in_widget_constructors
-  const ListTileOfWasteNotification({this.text = '', this.color = Colors.white, this.onChanged,
-      this.valueOfSwitch = false});
+  const ListTileOfWasteNotification(
+      {this.text = '',
+      this.color = Colors.white,
+      this.onChanged,
+      this.valueOfSwitch = false,
+      required this.wasteReminderTime,
+      required this.wasteSelectedDay});
   final String text;
   final Color color;
   final bool valueOfSwitch;
   final dynamic onChanged;
+  final TimeOfDay wasteReminderTime;
+  final String wasteSelectedDay;
   @override
   Widget build(BuildContext context) {
     return Container(
-      //width: MediaQuery.of(context).size.width/100*75,
+      width: MediaQuery.of(context).size.width * 0.9,
       //width: 200,
-      height: 45,
+      //height: 45,
       padding: const EdgeInsets.all(0),
       margin: const EdgeInsets.all(2),
       decoration: const BoxDecoration(
         borderRadius: kDRadius,
         color: kDBackgroundColorCalendar,
       ),
-      child: ListTile(
-        title: Text(
-          text,
-          style: TextStyle(
-              shadows: const [
-                Shadow(
-                    color: Colors.black12, offset: Offset(0, 0), blurRadius: 15)
-              ],
-              fontFamily: kDFontFamilyParagraph,
-              fontSize: 16,
-              color: color,
-              fontWeight: FontWeight.bold),
-        ),
-        leading: Icon(Icons.circle, color: color),
-        dense: true,
-        horizontalTitleGap: 0.0,
-        minVerticalPadding: 0.0,
-        style: ListTileStyle.list,
-        trailing: Switch(
-          value: valueOfSwitch,
-          onChanged: onChanged,
-        ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ListTile(
+            title: Text(
+              text,
+              style: TextStyle(
+                  shadows: const [
+                    Shadow(
+                        color: Colors.black12,
+                        offset: Offset(0, 0),
+                        blurRadius: 15)
+                  ],
+                  fontFamily: kDFontFamilyParagraph,
+                  fontSize: 16,
+                  color: color,
+                  fontWeight: FontWeight.bold),
+            ),
+            leading: Icon(Icons.circle, color: color),
+            dense: true,
+            horizontalTitleGap: 0.0,
+            minVerticalPadding: 0.0,
+            style: ListTileStyle.list,
+            trailing: Switch(
+              value: valueOfSwitch,
+              onChanged: onChanged,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              IconReminderOnOff(isSwitched: valueOfSwitch),
+              ReminderTimeAndDate(
+                  switcher: valueOfSwitch,
+                  wasteReminderTime: wasteReminderTime,
+                  wasteReminderDay: wasteSelectedDay),
+            ],
+          ),
+        ],
       ),
     );
   }
