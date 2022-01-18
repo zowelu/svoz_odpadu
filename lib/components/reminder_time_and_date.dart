f2f717ea6d03a98f8dbb3afeb74faac8b8907784
Lukas Zoubek <zoublu@email.cz>
2022-01-18 19:43:04 +0100

    Změna vzhledu reminder_time_and_date.dart

--- lib/components/reminder_time_and_date.dart
+++ lib/components/reminder_time_and_date.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:svoz_odpadu/components/text_normal.dart';
+import 'package:svoz_odpadu/variables/constants.dart';
 
 class ReminderTimeAndDate extends StatelessWidget {
-  const ReminderTimeAndDate({
-    Key? key, required this.switcher, required this.wasteReminderTime, required this.wasteReminderDay
-  }) : super(key: key);
+  const ReminderTimeAndDate(
+      {Key? key,
+      required this.switcher,
+      required this.wasteReminderTime,
+      required this.wasteReminderDay})
+      : super(key: key);
   final bool switcher;
   final TimeOfDay? wasteReminderTime;
   final String? wasteReminderDay;
@@ -16,13 +20,16 @@ class ReminderTimeAndDate extends StatelessWidget {
       fit: FlexFit.tight,
       child: Column(
         children: [
-          Text(
-            switcher ? wasteReminderDay! : '',
-            style: const TextStyle(color: Colors.white),
-            textAlign: TextAlign.center,
+          TextNormal(
+            text: switcher ? wasteReminderDay! : '',
+            color: kDBackgroundColor,
+            fontSize: 14,
           ),
           Center(
-            child: TextNormal(fontSize: 10,
+            child: TextNormal(
+              fontSize: 14,
+              color: kDBackgroundColor,
+              fontWeight: FontWeight.bold,
               text: switcher
                   ? '${wasteReminderTime?.hour.toString()} : ${wasteReminderTime?.minute.toString()}'
                   : 'Nenastaveno',
