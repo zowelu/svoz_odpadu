01f6151442ab1c5b1a764c211c479bdacf03ba05
zoublu <zoublu@email.cz>
2022-01-20 05:37:19 +0100

    vxtvořeno tlačítko na výis preference

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -501,6 +501,7 @@ class _SettingsPageState extends State<SettingsPage> {
                       ],
                     ),
                   ),
+                  ButtonSettings(onTap: (){sharedPreferencesGlobal.getAllPreferenceInMap();}, title: 'Get all preferences', subtitle: 'get', icon: Icons.arrow_circle_down),
                   ButtonSettings(
                       color: Colors.blueGrey,
                       onTap: () {
