68687de107ab4834106f50db5e4a585759048347
zoublu <zoublu@email.cz>
2022-01-25 04:25:55 +0100

    přidán fontSize do konstruktoru v text_header.dart

--- lib/components/text_header.dart
+++ lib/components/text_header.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:svoz_odpadu/variables/constants.dart';
 
 class TextHeader extends StatelessWidget {
-  const TextHeader({Key? key, required this.text, this.color = Colors.white}) : super(key: key);
+  const TextHeader({Key? key, required this.text, this.color = Colors.white, this.fontSize = kDFontSizeHeader}) : super(key: key);
 
   final String? text;
   final Color? color;
+  final double fontSize;
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text!,
       style: TextStyle(
-        fontSize: kDFontSizeHeader,
+        fontSize: fontSize,
         fontFamily: kDFontFamilyHeader,
         color: color,
       ),
