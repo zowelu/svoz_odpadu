909200e3dd8b18c455cc9dff70791dac90c863a4
Lukas Zoubek <zoublu@email.cz>
2022-01-26 09:32:57 +0100

    do konstruktoru text_header.dart přidán TextAlign

--- lib/components/text_header.dart
+++ lib/components/text_header.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:svoz_odpadu/variables/constants.dart';
 
 class TextHeader extends StatelessWidget {
-  const TextHeader({Key? key, required this.text, this.color = Colors.white, this.fontSize = kDFontSizeHeader}) : super(key: key);
+  const TextHeader({Key? key, required this.text, this.color = Colors.white, this.fontSize = kDFontSizeHeader, this.textAlign = TextAlign.start}) : super(key: key);
 
   final String? text;
   final Color? color;
   final double fontSize;
+  final TextAlign textAlign;
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +17,7 @@ class TextHeader extends StatelessWidget {
         fontSize: fontSize,
         fontFamily: kDFontFamilyHeader,
         color: color,
-      ),
+      ),textAlign: textAlign,
     );
   }
 }
