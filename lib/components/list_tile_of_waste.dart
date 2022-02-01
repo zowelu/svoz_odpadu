140fd627600881211ce58325c82c18171d966850
Lukas Zoubek <zoublu@email.cz>
2022-02-01 15:14:29 +0100

    upraven vzhled list_tile_of_waste.dart

--- lib/components/list_tile_of_waste.dart
+++ lib/components/list_tile_of_waste.dart
@@ -4,35 +4,48 @@ import 'package:svoz_odpadu/variables/constants.dart';
 
 class ListTileOfWaste extends StatelessWidget {
   // ignore: use_key_in_widget_constructors
-  const ListTileOfWaste(this.text, this.color);
-  final String text;
-  final Color color;
+  const ListTileOfWaste({this.text, this.date, this.color, this.dayOfWeek});
+  final String? text;
+  final Color? color;
+  final String? date;
+  final String? dayOfWeek;
 
   @override
   Widget build(BuildContext context) {
     return Container(
         margin: EdgeInsets.all(MediaQuery.of(context).size.height / 100 * 0.5),
         padding: const EdgeInsets.only(left: kDMargin, right: kDMargin),
-        height: MediaQuery.of(context).size.height / 100 * 5,
-        decoration: const BoxDecoration(
+        height: MediaQuery.of(context).size.height/100*10,
+        decoration: BoxDecoration(
             shape: BoxShape.rectangle,
-            borderRadius: kDRadiusSmall),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
+            borderRadius: kDRadiusSmall,
+            color: color),
+        child: Row(mainAxisSize: MainAxisSize.max,crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Icon(
-              Icons.circle,
-              color: color,
+            Expanded(
+              child: Column(crossAxisAlignment: CrossAxisAlignment.center,mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.circle,
+                    color: Colors.grey,
+                  ),
+                  TextNormal(
+                    text: text,
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ],
+              ),
             ),
-            const SizedBox(
-              width: 10,
-            ),
-            TextNormal(
-              text: text,
-              color: color,
-              fontSize: 14,fontWeight: FontWeight.bold,
-            ),
+            Expanded(
+              child: Column( crossAxisAlignment: CrossAxisAlignment.center,mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextNormal(text: date!),
+                  TextNormal(text: dayOfWeek!),
+                ],
+              ),
+            )
           ],
         ));
   }
