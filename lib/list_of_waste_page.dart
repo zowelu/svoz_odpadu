39b7b41e2ae252ec52c4c5ed3d48c61395ec6f6f
Lukas Zoubek <zoublu@email.cz>
2022-02-01 13:41:13 +0100

    úprava list_of_waste_page.dart

--- lib/list_of_waste_page.dart
+++ lib/list_of_waste_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:svoz_odpadu/components/my_appbar.dart';
 import 'package:svoz_odpadu/components/text_header.dart';
+import 'package:svoz_odpadu/variables/constants.dart';
+import 'package:svoz_odpadu/components/utils.dart';
 
 class ListOfWastePage extends StatefulWidget {
   const ListOfWastePage({Key? key}) : super(key: key);
@@ -12,6 +15,78 @@ class ListOfWastePage extends StatefulWidget {
 class _ListOfWastePageState extends State<ListOfWastePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(child: TextHeader(text: 'Ahoj',),);
+    return Scaffold(
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(kDMyAppBarHeight),
+        child: MyAppBar(),
+      ),
+      backgroundColor: kDBackgroundColor,
+      body: SingleChildScrollView(physics: const ScrollPhysics(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  color: kDBackgroundColorCalendar,
+                  width: double.infinity,
+                  height: kDMyAppBarHeight,
+                  child: const Center(
+                    child: TextHeader(
+                      text: 'Nastavení',
+                      color: kDBackgroundColor,
+                    ),
+                  ),
+                ),
+                Container(
+                  decoration: const BoxDecoration(
+                      color: kDBackgroundColorCalendar,
+                      borderRadius: kDRadiusLarge),
+                  padding: const EdgeInsets.all(2.0),
+                  margin:
+                      const EdgeInsets.only(left: kDMargin, right: kDMargin),
+                  child: ListView.builder(physics: const NeverScrollableScrollPhysics(),
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemCount: allWasteEvents.length,
+                    itemBuilder: (context, int index) {
+                      String date =
+                          allWasteEvents.keys.elementAt(index).toString();
+
+                      String name = allWasteEvents.values.elementAt(index).toString();
+                      String nameOfWaste = name.substring(1,name.length-1);
+                      if(nameOfWaste == 'plast'){
+                        nameOfWaste = 'Plast a nápojový karton\nDrobné kovy';
+                      } else if(nameOfWaste == 'směsný' || nameOfWaste == ('směsný odpad')){
+                        nameOfWaste = 'Směsný odpad';
+                      }else if(nameOfWaste == 'papír'){
+                        nameOfWaste = 'Papírový odpad';
+                      }else if(nameOfWaste == 'bio'){
+                        nameOfWaste = 'Bio odpad';
+                      }
+
+                      return Column(
+                        children: [
+                          ListTile(
+                            title: TextHeader(text: nameOfWaste, color: kDBackgroundColor,),
+                            subtitle: Text(date),
+                          ),
+                          const Divider(
+                            height: 2.0,
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
