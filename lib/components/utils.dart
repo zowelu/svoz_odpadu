c59f433f6540eb82ce7efca71e55fecc9adaf8cb
Lukas Zoubek <zoublu@email.cz>
2022-02-01 08:02:39 +0100

    zakomentování obsahu mapOfWaste, přejmenování map, zakomentování třídy Event(ta je nyní v calendarData)

--- lib/components/utils.dart
+++ lib/components/utils.dart
@@ -1,22 +1,15 @@
 import 'dart:collection';
-
-//vytažení dat z Map
-Map<DateTime, List<Event>> plasticWasteEvents = _plasticWasteEvents;
-Map<DateTime, List<Event>> bioWasteEvents = _bioWasteEvents;
-Map<DateTime, List<Event>> paperWasteEvents = _paperWasteEvents;
-Map<DateTime, List<Event>> mixedWasteEvents = _mixedWasteEvents;
-
-
+import 'package:svoz_odpadu/components/calendar_data.dart';
 
 /// Example event class.
-class Event {
+/*class Event {
   final String title;
 
   const Event(this.title);
 
   @override
   String toString() => title;
-}
+}*/
 
 /// Example events.
 ///
@@ -31,21 +24,19 @@ int getHashCode(DateTime key) {
   return key.day * 1000000 + key.month * 10000 + key.year;
 }
 
-
 /// nakopírování všech Map do jednoho
 Map getAllEventsToMap() {
-  allWasteEvents.addAll(_mixedWasteEvents);
-  allWasteEvents.addAll(_plasticWasteEvents);
-  allWasteEvents.addAll(_paperWasteEvents);
-  allWasteEvents.addAll(_bioWasteEvents);
+  allWasteEvents.addAll(mixedWasteEvents);
+  allWasteEvents.addAll(plasticWasteEvents);
+  allWasteEvents.addAll(paperWasteEvents);
+  allWasteEvents.addAll(bioWasteEvents);
   return allWasteEvents;
 }
 
 Map<DateTime, List<Event>> allWasteEvents = {};
 
-
-Map<DateTime, List<Event>> _mixedWasteEvents = {
-  DateTime(2022, 1, 12): [const Event('Směsný odpad')],
+Map<DateTime, List<Event>> mixedWasteEvents = {
+  /*DateTime(2022, 1, 12): [const Event('Směsný odpad')],
   DateTime(2022, 2, 9): [const Event('Směsný odpad')],
   DateTime(2022, 3, 9): [const Event('Směsný odpad')],
   DateTime(2022, 4, 6): [const Event('Směsný odpad')],
@@ -57,11 +48,11 @@ Map<DateTime, List<Event>> _mixedWasteEvents = {
   DateTime(2022, 9, 21): [const Event('Směsný odpad')],
   DateTime(2022, 10, 19): [const Event('Směsný odpad')],
   DateTime(2022, 11, 16): [const Event('Směsný odpad')],
-  DateTime(2022, 12, 14): [const Event('Směsný odpad')],
+  DateTime(2022, 12, 14): [const Event('Směsný odpad')],*/
 };
 
-Map<DateTime, List<Event>> _plasticWasteEvents = {
-  DateTime(2022, 1, 5): [ const Event('Plast a nápojový karton, Drobné kovy')],
+Map<DateTime, List<Event>> plasticWasteEvents = {
+  /*DateTime(2022, 1, 5): [ const Event('Plast a nápojový karton, Drobné kovy')],
   DateTime(2022, 1, 19): [const Event('Plast a nápojový karton, Drobné kovy')],
   DateTime(2022, 2, 2): [const Event('Plast a nápojový karton, Drobné kovy')],
   DateTime(2022, 2, 16): [const Event('Plast a nápojový karton, Drobné kovy')],
@@ -86,11 +77,11 @@ Map<DateTime, List<Event>> _plasticWasteEvents = {
   DateTime(2022, 11, 9): [const Event('Plast a nápojový karton, Drobné kovy')],
   DateTime(2022, 11, 23): [const Event('Plast a nápojový karton, Drobné kovy')],
   DateTime(2022, 12, 7): [const Event('Plast a nápojový karton, Drobné kovy')],
-  DateTime(2022, 12, 21): [const Event('Plast a nápojový karton, Drobné kovy')],
+  DateTime(2022, 12, 21): [const Event('Plast a nápojový karton, Drobné kovy')],*/
 };
 
-Map<DateTime, List<Event>> _bioWasteEvents = {
-  DateTime(2022, 1, 5): [ const Event('Bioodpad')],
+Map<DateTime, List<Event>> bioWasteEvents = {
+  /* DateTime(2022, 1, 5): [ const Event('Bioodpad')],
   DateTime(2022, 2, 2): [ const Event('Bioodpad')],
   DateTime(2022, 3, 2): [const Event('Bioodpad')],
   DateTime(2022, 3, 30): [ const Event('Bioodpad')],
@@ -111,11 +102,11 @@ Map<DateTime, List<Event>> _bioWasteEvents = {
   DateTime(2022, 10, 26): [const Event('Bioodpad')],
   DateTime(2022, 11, 9): [const Event('Bioodpad')],
   DateTime(2022, 11, 23): [const Event('Bioodpad')],
-  DateTime(2022, 12, 7): [const Event('Bioodpad')],
+  DateTime(2022, 12, 7): [const Event('Bioodpad')],*/
 };
 
-Map<DateTime, List<Event>> _paperWasteEvents = {
-  DateTime(2021, 12, 29): [const Event('Papír')],
+Map<DateTime, List<Event>> paperWasteEvents = {
+  /*DateTime(2021, 12, 29): [const Event('Papír')],
   DateTime(2022, 1, 26): [const Event('Papír')],
   DateTime(2022, 2, 23): [const Event('Papír')],
   DateTime(2022, 3, 23): [const Event('Papír')],
@@ -128,7 +119,7 @@ Map<DateTime, List<Event>> _paperWasteEvents = {
   DateTime(2022, 10, 5): [const Event('Papír')],
   DateTime(2022, 11, 2): [const Event('Papír')],
   DateTime(2022, 11, 30): [const Event('Papír')],
-  DateTime(2022, 12, 28): [const Event('Papír')],
+  DateTime(2022, 12, 28): [const Event('Papír')],*/
 };
 
 /// Checks if two DateTime objects are the same day.
@@ -140,4 +131,3 @@ bool isSameDay(DateTime? a, DateTime? b) {
 
   return a.year == b.year && a.month == b.month && a.day == b.day;
 }
-
