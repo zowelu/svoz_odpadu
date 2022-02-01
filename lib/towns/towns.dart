b0b508035fb4696f3823cf22c0f54273d74a5dc2
Lukas Zoubek <zoublu@email.cz>
2022-02-01 08:03:10 +0100

    vytvoření towns.dart a třídy Dolní Kounice

---
+++ lib/towns/towns.dart
@@ -0,0 +1,21 @@
+
+
+class DolniKounice {
+  String? name = 'Dolní Kounice';
+  String? iconPath = 'assets/images/DK_znak_800px.png';
+
+  //map pro získání index a zároveň odkazy na calendarID
+  Map<String, String> calendarID = {
+    'směsný': '13r56ftkqvl368a14fimn1ifc4@group.calendar.google.com',
+    'plast': 'go5dkg6cnflo277vhc6cbemt3k@group.calendar.google.com',
+    'papír': 'p7g0np51igvv0bko1bf4nmtmf0@group.calendar.google.com',
+    'bioodpad': 'bnjcsj8qmn2guo40789odlvrvo@group.calendar.google.com'
+  };
+
+  /*void createTownObject() {
+    Town dolniKounice = new Town();
+    dolniKounice.name = 'Dolní Kounice';
+    dolniKounice.iconPath = 'assets/images/DK_znak_800px.png';
+  }*/
+  //DolniKounice({this.name, this.iconPath,});
+}
