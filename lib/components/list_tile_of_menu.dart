2e8c908eea4f3e4de1e82c549ea02f6642937a3c
Lukas Zoubek <zoublu@email.cz>
2022-02-05 16:24:42 +0100

    úprava list_tile_of_menu.dart

--- lib/components/list_tile_of_menu.dart
+++ lib/components/list_tile_of_menu.dart
@@ -6,29 +6,31 @@ import 'package:svoz_odpadu/components/divider_menu.dart';
 class ListTileOfMenu extends StatelessWidget {
   const ListTileOfMenu({
     Key? key,
-    required this.fontSize,
+    this.fontSize = 14, required this.text, required this.onTap, this.iconSize = 30, this.colorOfTile = kDBackgroundColorCalendar
   }) : super(key: key);
 
   final double fontSize;
+  final String text;
+  final onTap;
+  final double iconSize;
+  final Color colorOfTile;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        ListTile(
-          leading: const Icon(
+        ListTile(tileColor: colorOfTile,
+          leading: Icon(
             Icons.perm_contact_calendar_outlined,
-            size: 30,
+            size: iconSize,
             color: kDBackgroundColor,
           ),
           title: TextHeader(
-            text: 'Kalendář',
+            text: text,
             color: kDBackgroundColor,
             fontSize: fontSize,
           ),
-          onTap: () {
-            Navigator.of(context).pop();
-          },
+          onTap: onTap,
         ),
         DividerMenu(),
       ],
