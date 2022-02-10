56e9c9aa8afdf478a9f398c963c30a46205b4d2d
Lukas Zoubek <zoublu@email.cz>
2022-02-10 19:15:48 +0100

    ¨změna drawer

--- lib/components/drawer_menu.dart
+++ lib/components/drawer_menu.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:svoz_odpadu/about_app_page.dart';
 import 'package:svoz_odpadu/city_picker_page.dart';
+import 'package:svoz_odpadu/components/button_settings.dart';
 import 'package:svoz_odpadu/components/divider_menu.dart';
 import 'package:svoz_odpadu/components/list_tile_of_menu.dart';
 import 'package:svoz_odpadu/components/open_url_in_browser.dart';
@@ -17,99 +18,110 @@ class DrawerMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return 
-      Drawer(
-        backgroundColor: kDBackgroundColorCalendar,
-        child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      decoration: const BoxDecoration(boxShadow: [
-                        BoxShadow(
-                          color: kDBoxShadowColor,
-                          offset: Offset(2, 0),
-                          blurRadius: 10,
-                        ),
-                      ]),
-                      child: DrawerHeader(
-                        decoration: const BoxDecoration(color: kDBackgroundColor),
-                        padding: const EdgeInsets.all(0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            Image.asset(
-                              'assets/images/app_icon.png',
-                              height: 60,
-                            ),
-                            const TextHeader(
-                              text: 'Svoz odpadu',
-                              textAlign: TextAlign.center,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Container(
-                      color: kDBackgroundColor,
-                      width: double.infinity,
-                      padding: EdgeInsets.all(0),
+    return Drawer(
+      backgroundColor: kDBackgroundColorCalendar,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    // decoration: const BoxDecoration(boxShadow: [
+                    //   BoxShadow(
+                    //     color: kDBoxShadowColor,
+                    //     offset: Offset(2, 0),
+                    //     blurRadius: 10,
+                    //   ),
+                    // ]),
+                    child: DrawerHeader(
+                      decoration: const BoxDecoration(color: kDBackgroundColor),
                       child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: [
-                          Image.asset('assets/images/DK_znak_800px.png', width: 60),
-                          TextHeader(text: 'Dolní Kounice'),
+                          Image.asset(
+                            'assets/images/app_icon.png',
+                            height: 100,
+                          ),
+                          const TextHeader(
+                            text: 'Svoz odpadu',
+                            textAlign: TextAlign.center,
+                          ),
                         ],
                       ),
                     ),
-                    Column(
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                    child: Column(
                       children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const DividerMenu(),
-                            ListTileOfMenu(
-                                text: 'Kalendář',
-                                onTap: () {
-                                  Navigator.of(context).pop();
-                                }),
-                            ListTileOfMenu(
-                                text: 'Vybrat město/obec',
-                                onTap: () {
-                                  Navigator.popAndPushNamed(
-                                      context, CityPickerPage.id);
-                                }),
-                            ListTileOfMenu(
-                              text: 'Nastavení',
-                              onTap: () {
-                                Navigator.popAndPushNamed(context, SettingsPage.id);
-                              },
-                            ),
-                            ListTileOfMenu(
-                              text: 'Ohodnotit',
-                              onTap: () {},
-                            ),
-                            ListTileOfMenu(
-                              text: 'O aplikaci',
-                              onTap: () {
-                                Navigator.popAndPushNamed(context, AboutAppPage.id);
-                              },
-                            ),
-                          ],
-                        )
+                        Image.asset('assets/images/DK_znak_800px.png',
+                            width: 60),
+                        TextHeader(
+                          text: 'Dolní Kounice',
+                          color: kDBackgroundColor,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.popAndPushNamed(
+                                context, CityPickerPage.id);
+                          },
+                          child: Text(
+                            'Vybrat jiné město',
+                            style: TextStyle(
+                                fontFamily: kDFontFamilyHeader,
+                                color: Colors.white),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              primary: kDBackgroundColor),
+                        ),
                       ],
                     ),
-                    /*Container(
+                  ),
+                  Column(
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const DividerMenu(),
+                          ListTileOfMenu(
+                              text: 'Kalendář',
+                              onTap: () {
+                                Navigator.of(context).pop();
+                              }),
+                          ListTileOfMenu(
+                            text: 'Nastavení',
+                            onTap: () {
+                              Navigator.popAndPushNamed(
+                                  context, SettingsPage.id);
+                            },
+                          ),
+                          ListTileOfMenu(
+                            text: 'Ohodnotit',
+                            onTap: () {},
+                          ),
+                          ListTileOfMenu(
+                            text: 'O aplikaci',
+                            onTap: () {
+                              Navigator.popAndPushNamed(
+                                  context, AboutAppPage.id);
+                            },
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  /*Container(
                       decoration:
                           const BoxDecoration(color: kDBackgroundColor, boxShadow: [
                         BoxShadow(
@@ -166,12 +178,12 @@ class DrawerMenu extends StatelessWidget {
                         ],
                       ),
                     ),*/
-                  ],
-                ),
-              ],
-            ),
+                ],
+              ),
+            ],
           ),
         ),
+      ),
     );
   }
 }
