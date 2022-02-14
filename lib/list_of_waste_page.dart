b1b55c17d3c356cc844b3d219a79f561408daa44
zoublu <zoublu@email.cz>
2022-02-14 04:26:31 +0100

    Odstranění scaffold

--- lib/list_of_waste_page.dart
+++ lib/list_of_waste_page.dart
@@ -27,97 +27,89 @@ class _ListOfWastePageState extends State<ListOfWastePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const PreferredSize(
-        preferredSize: Size.fromHeight(kDMyAppBarHeight),
-        child: MyAppBar(),
-      ),
-      backgroundColor: kDBackgroundColor,
-      body: SingleChildScrollView(
-        physics: const ScrollPhysics(),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  color: kDBackgroundColorCalendar,
-                  width: double.infinity,
-                  height: kDMyAppBarHeight,
-                  child: const Center(
-                    child: TextHeader(
-                      text: 'Přehled',
-                      color: kDBackgroundColor,
-                    ),
+    return SingleChildScrollView(
+      physics: const ScrollPhysics(),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                color: kDBackgroundColorCalendar,
+                width: double.infinity,
+                height: kDMyAppBarHeight,
+                child: const Center(
+                  child: TextHeader(
+                    text: 'Přehled',
+                    color: kDBackgroundColor,
                   ),
                 ),
-                const SizedBox(
-                  height: kDMargin,
+              ),
+              const SizedBox(
+                height: kDMargin,
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                    color: kDBackgroundColorCalendar,
+                    borderRadius: kDRadiusLarge),
+                padding: const EdgeInsets.all(2.0),
+                margin: const EdgeInsets.only(left: kDMargin, right: kDMargin),
+                child: ListView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: allWasteEventsOverviewListFromNow.length,
+                  itemBuilder: (context, int index) {
+                    Color? color;
+                    String? asset;
+                    DateTime date =
+                        allWasteEventsOverviewListFromNow.keys.elementAt(index);
+
+                    DateFormat formatter = DateFormat('dd. MM. yyyy');
+                    String dateFormatted = formatter.format(date);
+
+                    String dayOfWeek = daysOfWeek[date.weekday]!;
+
+                    String name = allWasteEventsOverviewListFromNow.values
+                        .elementAt(index)
+                        .toString();
+                    String nameOfWaste = name.substring(1, name.length - 1);
+                    if (nameOfWaste == 'plast') {
+                      nameOfWaste = 'Plast a nápojový karton,\nDrobné kovy';
+                      color = kDColorWastePlastic;
+                      asset = 'assets/images/icons/plastic.png';
+                    } else if (nameOfWaste == 'směsný' ||
+                        nameOfWaste == ('směsný odpad')) {
+                      nameOfWaste = 'Směsný odpad';
+                      color = kDColorWasteMixed;
+                      asset = 'assets/images/icons/mixed.png';
+                    } else if (nameOfWaste == 'papír') {
+                      nameOfWaste = 'Papírový odpad';
+                      asset = 'assets/images/icons/paper.png';
+                      color = kDColorWastePaper;
+                    } else if (nameOfWaste == 'bio' ||
+                        nameOfWaste == 'bioodpad') {
+                      nameOfWaste = 'Bioodpad';
+                      color = kDColorWasteBio;
+                      asset = 'assets/images/icons/bio.png';
+                    }
+
+                    return ListTileOfWaste(
+                      text: nameOfWaste,
+                      color: color,
+                      date: dateFormatted,
+                      dayOfWeek: dayOfWeek,
+                      asset: asset,
+                    );
+                  },
                 ),
-                Container(
-                  decoration: const BoxDecoration(
-                      color: kDBackgroundColorCalendar,
-                      borderRadius: kDRadiusLarge),
-                  padding: const EdgeInsets.all(2.0),
-                  margin:
-                      const EdgeInsets.only(left: kDMargin, right: kDMargin),
-                  child: ListView.builder(
-                    physics: const NeverScrollableScrollPhysics(),
-                    scrollDirection: Axis.vertical,
-                    shrinkWrap: true,
-                    itemCount: allWasteEventsOverviewListFromNow.length,
-                    itemBuilder: (context, int index) {
-                      Color? color;
-                      String? asset;
-                      DateTime date = allWasteEventsOverviewListFromNow.keys
-                          .elementAt(index);
-
-                      DateFormat formatter = DateFormat('dd. MM. yyyy');
-                      String dateFormatted = formatter.format(date);
-
-                      String dayOfWeek = daysOfWeek[date.weekday]!;
-
-                      String name = allWasteEventsOverviewListFromNow.values
-                          .elementAt(index)
-                          .toString();
-                      String nameOfWaste = name.substring(1, name.length - 1);
-                      if (nameOfWaste == 'plast') {
-                        nameOfWaste = 'Plast a nápojový karton,\nDrobné kovy';
-                        color = kDColorWastePlastic;
-                        asset = 'assets/images/icons/plastic.png';
-                      } else if (nameOfWaste == 'směsný' ||
-                          nameOfWaste == ('směsný odpad')) {
-                        nameOfWaste = 'Směsný odpad';
-                        color = kDColorWasteMixed;
-                        asset = 'assets/images/icons/mixed.png';
-                      } else if (nameOfWaste == 'papír') {
-                        nameOfWaste = 'Papírový odpad';
-                        asset = 'assets/images/icons/paper.png';
-                        color = kDColorWastePaper;
-                      } else if (nameOfWaste == 'bio' ||
-                          nameOfWaste == 'bioodpad') {
-                        nameOfWaste = 'Bioodpad';
-                        color = kDColorWasteBio;
-                        asset = 'assets/images/icons/bio.png';
-                      }
-
-                      return ListTileOfWaste(
-                        text: nameOfWaste,
-                        color: color,
-                        date: dateFormatted,
-                        dayOfWeek: dayOfWeek,
-                        asset: asset,
-                      );
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
