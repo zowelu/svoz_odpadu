554253347c070a5d8756970cb466cf44ea3d8fd9
zoublu <zoublu@email.cz>
2022-02-14 05:15:17 +0100

    Úprava my_appbar

--- lib/components/my_appbar.dart
+++ lib/components/my_appbar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:svoz_odpadu/city_picker_page.dart';
 import 'package:svoz_odpadu/variables/constants.dart';
+import 'package:svoz_odpadu/variables/global_var.dart';
 
 class MyAppBar extends StatelessWidget {
   const MyAppBar({Key? key}) : super(key: key);
@@ -10,30 +12,27 @@ class MyAppBar extends StatelessWidget {
       centerTitle: true,
       backgroundColor: kDBackgroundColor,
       //bottom: PreferredSize(child: TextWidget(), preferredSize: Size.fromHeight(4.0)),
-      title: RichText(
-        textAlign: TextAlign.center,
-        text: const TextSpan(
-          children: [
-            WidgetSpan(
-              child: Text(
-                'KALENDÁŘ SVOZU ODPADU',
-                style: TextStyle(
-                    fontFamily: kDFontFamilyHeader,
-                    fontSize: 18,
-                    color: kDColorTextColorBackground),
-              ),
-            ),
-            WidgetSpan(
-              child: Text(
-                'Město Dolní Kounice',
-                style: TextStyle(
-                    fontFamily: kDFontFamilyHeader,
-                    fontSize: 13,
-                    color: kDColorTextColorBackground),
-              ),
-            ),
-          ],
-        ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/DK_znak_200px.png',
+            width: 50,
+          ),
+          Text(
+            'Město Dolní Kounice',
+            style: TextStyle(
+                fontFamily: kDFontFamilyHeader,
+                fontSize: 18,
+                color: kDColorTextColorBackground),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, CityPickerPage.id);
+              },
+              child: Text('Vybrat jiné'))
+        ],
       ),
     );
   }
