b9b4aab380d67b9e744e3878688ee5dbd76dcd66
Lukáš Zoubek <zoubek@datasys.cz>
2022-12-02 21:05:43 +0100

    upgrade

---
+++ lib/widgets/favorite_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FavoriteColors {
+  Color backgroundColor = const Color(0xFF004466);
+  Color backgroundColorCalendar = const Color(0xFFD2D2D2);
+  Color colorTextWhite = Colors.white;
+  Color colorTextColorBackground = const Color(0xFFFFFFFF);
+  Color textBackgroundTransparent = const Color(0x99000000);
+  Color boxShadowColor = const Color(0xFF2C3E50);
+  Color colorOfBackgroundHeader = const Color(0xB3FFFFFF);
+  Color colorFloatingButton = const Color(0xFFA63D40);
+  Color colorPlayerBackground = const Color(0xFFE9B872);
+  Color colorPlayerButton = const Color(0xFFA63D40);
+  Color colorWastePlastic = Colors.orangeAccent;
+  Color colorWasteBio = Colors.brown;
+  Color colorWastePaper = Colors.blueAccent;
+  Color colorWasteMixed = Colors.black87;
+}
