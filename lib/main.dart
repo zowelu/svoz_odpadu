ae8d17011e50b0ad4980082e83b61a58cfb5f30d
Lukáš Zoubek <zoubek@datasys.cz>
2022-12-02 21:26:31 +0100

    vytvoření home_page.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:svoz_odpadu/widgets/favorite_colors.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'package:svoz_odpadu/screens/home_page.dart';
 
 void main() async {
   initializeDateFormatting().then((_) => runApp(MyApp()));
@@ -25,122 +24,6 @@ class MyApp extends StatelessWidget {
           )),
       debugShowCheckedModeBanner: false,
       home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SizedBox(
-        child: TableCalendar(
-          rowHeight: MediaQuery.of(context).size.height / 100 * 6,
-          eventLoader: (day) {
-            return [DateTime(2022, 11, 6)];
-            /*_getEventsForDay(day)*/;
-          },
-          calendarFormat: CalendarFormat.month,
-          startingDayOfWeek: StartingDayOfWeek.monday,
-          focusedDay: DateTime(2022, 11, 6),
-          firstDay: DateTime(2022, 01, 01),
-          lastDay: DateTime(2022, 12, 31),
-          locale: 'cs_CZ',
-          calendarStyle: CalendarStyle(
-            markerSizeScale: 1.35,
-            canMarkersOverflow: true,
-            outsideDaysVisible: false,
-            markersMaxCount: 5,
-            isTodayHighlighted: true,
-            cellPadding: const EdgeInsets.all(0),
-            cellMargin: const EdgeInsets.all(0),
-            defaultTextStyle: const TextStyle(
-                color: Colors.black, fontWeight: FontWeight.bold),
-            todayTextStyle: const TextStyle(
-                fontWeight: FontWeight.bold, color: Colors.white),
-            todayDecoration: BoxDecoration(
-              color: FavoriteColors().backgroundColor,
-              borderRadius: const BorderRadius.all(
-                Radius.circular(10.0),
-              ),
-            ),
-            weekendTextStyle: const TextStyle(
-              color: Colors.grey,
-            ),
-          ),
-          headerStyle: HeaderStyle(
-            leftChevronIcon: Icon(Icons.arrow_back_ios_new_rounded,
-                color: FavoriteColors().backgroundColor),
-            rightChevronIcon: Icon(Icons.arrow_forward_ios_rounded,
-                color: FavoriteColors().backgroundColor),
-            formatButtonVisible: false,
-            titleCentered: true,
-            titleTextStyle: TextStyle(
-              color: FavoriteColors().backgroundColor,
-            ),
-          ),
-          daysOfWeekStyle: const DaysOfWeekStyle(
-            weekdayStyle: TextStyle(color: Colors.blueGrey),
-            weekendStyle: TextStyle(color: Colors.blueGrey),
-          ),
-          calendarBuilders: CalendarBuilders(
-            singleMarkerBuilder: (context, date, event) {
-              Map<String, Color> colorsOfWaste = {
-                'plast': FavoriteColors().colorWastePlastic,
-                'papír': FavoriteColors().colorWastePaper,
-                'směsný odpad': FavoriteColors().colorWasteMixed,
-                'bioodpad': FavoriteColors().colorWasteBio
-              };
-              Color cor = Colors.pink;
-              if (event.toString() == 'Plast'.toLowerCase()) {
-                cor = colorsOfWaste['plast']!;
-              }
-              if (event.toString() == 'Papír'.toLowerCase()) {
-                cor = colorsOfWaste['papír']!;
-              }
-              if (event.toString() == 'Směsný odpad'.toLowerCase()) {
-                cor = colorsOfWaste['směsný odpad']!;
-              }
-              if (event.toString() == 'Bio'.toLowerCase()) {
-                cor = colorsOfWaste['bioodpad']!;
-              }
-              double size = 14;
-              return Container(
-                margin: const EdgeInsets.only(bottom: 100),
-                child: Icon(
-                  Icons.circle,
-                  color: cor,
-                  size: size,
-                ),
-              );
-            },
-            /*singleMarkerBuilder: (context, day, event) {
-                        //DateTime dayRaw = day;
-                        DateFormat dateFormat = DateFormat('d');
-                        String dayString = dateFormat.format(day);
-                        Widget? children;
-                        if (event.toString() == 'Směsný odpad') {
-                          children =
-                              MarkerEvent(kDColorWasteMixed, dayString);
-                        } else if (event.toString() == 'Papír') {
-                          children =
-                              MarkerEvent(kDColorWastePaper, dayString);
-                        } else if (event.toString() ==
-                            'Plast a nápojový karton, Drobné kovy') {
-                          children =
-                              MarkerEvent(kDColorWastePlastic, dayString);
-                        } else if (event.toString() == 'Bioodpad') {
-                          children = MarkerEventGradient(
-                              const [kDColorWastePlastic, kDColorWasteBio],
-                              dayString);
-                        }
-                        return children;
-                      },*/
-          ),
-        ),
-      ),
     );
   }
 }
