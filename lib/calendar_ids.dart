1b0fad7042722f69c10d317e65d232ddbdf7dbd0
Lukáš Zoubek <zoubek@datasys.cz>
2022-12-04 21:20:34 +0100

    vytvoření calendar_id.dart a přesun

---
+++ lib/calendar_ids.dart
@@ -0,0 +1,8 @@
+class CalendarIds {
+  Map<String, String> calendarIds = {
+    'mixed': 'go5dkg6cnflo277vhc6cbemt3k@group.calendar.google.com',
+    'plastic': 'go5dkg6cnflo277vhc6cbemt3k@group.calendar.google.com',
+    'paper': 'p7g0np51igvv0bko1bf4nmtmf0@group.calendar.google.com',
+    'bio': 'bnjcsj8qmn2guo40789odlvrvo@group.calendar.google.com'
+  };
+}
--- lib/services/calendar_service.dart
+++ lib/services/calendar_service.dart
@@ -2,21 +2,18 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:logging/logging.dart';
+import 'package:svoz_odpadu/calendar_ids.dart';
 
 class CalendarService {
   final Logger _log = Logger('CalendarService');
-  Map<String, String> calendarId = {
-    'mixed': 'go5dkg6cnflo277vhc6cbemt3k@group.calendar.google.com',
-    'plastic': 'go5dkg6cnflo277vhc6cbemt3k@group.calendar.google.com',
-    'paper': 'p7g0np51igvv0bko1bf4nmtmf0@group.calendar.google.com',
-    'bio': 'bnjcsj8qmn2guo40789odlvrvo@group.calendar.google.com'
-  };
+
+  final CalendarIds _calendarIds = CalendarIds();
 
   /// Vrátí všechny
   Future<List<Map<String, dynamic>>> getCalendarConfirmedData() async {
     /// List položek kalendáře se statusem 'confirmed'.
     List<Map<String, dynamic>> confirmedItems = [];
-    for (String calendarId in calendarId.values) {
+    for (String calendarId in _calendarIds.calendarIds.values) {
       final uri = Uri.parse(
           'https://www.googleapis.com/calendar/v3/calendars/$calendarId/events?key=AIzaSyB6op7SkNQjom3XP8sZkwhYkxew3vhdoKg');
       _log.info('uri: $uri');
